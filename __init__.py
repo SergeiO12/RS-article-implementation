4b827624fbb072bdb88828468656d8af10943cda
Orlov Sergei <orlov.sv@phystech.edu>
2024-05-03 17:46:03 +0300

    one more

--- __init__.py
+++
@@ -1,6 +0,0 @@
-def get_task(name):
-    if name == 'rec_try':
-        from rec_task import MovieLensRecommendationTask
-        return MovieLensRecommendationTask()
-    else:
-        raise NotImplementedError
